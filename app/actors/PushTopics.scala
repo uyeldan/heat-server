40961dfb0abe033f94942214734d9a57636c05dc
Dennis de Klerk <dmdeklerk@gmail.com>
2016-09-05 21:51:08 +0200

    Whitespace

--- app/actors/PushTopics.scala
+++ app/actors/PushTopics.scala
@@ -1,3 +1,25 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Heat Ledger Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
 package actors
 
 import play.api.libs.json._
@@ -5,17 +27,16 @@ import heat.util.Convert
 import models.ReplicatorMessage
 
 object PushTopics {
-  
+
   def message(message: ReplicatorMessage.Model) = {
-    val json = ReplicatorMessage.toJson(message)    
+    val json = ReplicatorMessage.toJson(message)
     WebSocketActorManager.push(WebSocketActorEvent(message.senderId, "MESSAGE", Map(), json))
-    WebSocketActorManager.push(WebSocketActorEvent(message.recipientId, "MESSAGE", Map(), json))      
+    WebSocketActorManager.push(WebSocketActorEvent(message.recipientId, "MESSAGE", Map(), json))
   }
-  
+
   def messageUpdate(message: ReplicatorMessage.Model) = {
     val json = ReplicatorMessage.toJson(message)
     WebSocketActorManager.push(WebSocketActorEvent(message.senderId, "MESSAGE-UPDATE", Map(), json))
-    WebSocketActorManager.push(WebSocketActorEvent(message.recipientId, "MESSAGE-UPDATE", Map(), json))    
+    WebSocketActorManager.push(WebSocketActorEvent(message.recipientId, "MESSAGE-UPDATE", Map(), json))
   }
-  
 }
